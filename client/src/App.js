c9396b48acfa35e39115fa8b6ea3baa18a95028a
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-08-04 01:36:21 -0700

    basic react routes created

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import './App.css';
 
@@ -9,11 +12,15 @@ function App() {
     <Router>
       <Navigation />
 
-      <Switch>
-        {/* <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='*' component={Home} /> */}
-      </Switch>
+      <Container>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='*' render={() => {
+            return <h1>404</h1>;
+          }} />
+        </Switch>
+      </Container>
     </Router>
   );
 }
